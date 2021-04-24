ac472cc74660bcac92c98052374d7989d9afe91b
Ivan <44531255+vibeRunner@users.noreply.github.com>
2021-04-25 01:43:32 +0200

    Create uh.cpp

---
+++ uh.cpp
@@ -0,0 +1,41 @@
+// by viberunner
+
+// polecenie do zadania nie było dobrze wyjaśnione
+// ale zakładając, że program ma wypisywać dla wartości 5:
+//
+// 2,
+// 4,5
+// 8,9,10
+// 16,17,18,19
+// 32,33,34,35,36
+//
+//  |                    -> w bok o jeden więcej liczb w kolejności
+// \|/ w dół potęgi liczby 2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+        int h;
+        cin >> h;
+        int now = 1;
+
+	cout << endl;
+
+        for (int i = 0; i < h; i++){
+
+                now *= 2;
+
+                for (int row = 0; row <= i; row++){
+
+                        cout << (now + row) << ',';
+
+                }
+
+                cout << endl;
+
+        }
+
+        return 0;
+}
